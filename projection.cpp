fb9a0fcfe1d65360bb374ef4dbe062a8ae0fe796
Lan-sx <shexin@ihep.ac.cn>
2024-06-25 11:14:30 +0800

    update GuiTable and main function

--- projection.cpp
+++ projection.cpp
@@ -14,12 +14,13 @@
 #include <TVector3.h>
 #include <TList.h>
 #include <TSystem.h>
-
+#include <TCanvas.h>
 
 #include "geoEveViewer.h"
 #include "MultiView.h"
 #include "RunManager.h"
 #include "GuiTable.h"
+#include "Lansxlogon.h"
 
 #include <vector>
 #include <iostream>
@@ -31,7 +32,7 @@ using namespace std;
 
 void extractGentle()
 {
-    //geoEveViewer::ExtractGentleVolume("../../geodata/TPC_ModularEndcap_o1_v01_MMCF.root", "../../geodata/TPC_ModularEndcap_o1_v01_MMCF_Extract.root",4);
+    //geoEveViewer::ExtractGentleVolume("../../geodata/TPC_Simple_o1_v01_MM_CF_bugfix.root", "../../geodata/TPC_Simple_o1_v01_MM_CF_bugfix_Extract.root",6);
     //geoEveViewer::ExtractGentleVolume("./TPC_Simple_TDR_o1_v01_mm.root", "./TPC_Simple_TDR_o1_v01_mmExtract.root", 4);
 }
 
@@ -40,18 +41,23 @@ int main(int argc, char** argv)
 {
     TApplication app("app",&argc,argv);
     //TRint app("app",&argc,argv);
-    //test01();
-    geoEveViewer *g = new geoEveViewer("../../geodata/TPC_ModularEndcap_o1_v01_MMCF.root", "../../geodata/TPC_ModularEndcap_o1_v01_MMCF_Extract.root", 80., kTRUE);
+    //extractGentle();
+    LansxFormat::myStyle();
+    geoEveViewer *g = new geoEveViewer("../../geodata/TPC_ModularEndcap_o1_v01_MM_CF_bugfix.root", "../../geodata/TPC_ModularEndcap_o1_v01_MM_CF_bugfix_Extract.root", 80., kTRUE);
     g->MakeMultiViewer();
-    
-    GuiTable* gui = new GuiTable(g->Get_EveManager());
-    
-    //g->MakeLeftGUI();
-    
-    //TVector3 p0(0., 160., -140.), p1(0., 1., 0.);
-    //auto vecX0tables1 = g->Start_Track(p0, p1, kFALSE);
-    
+    //TVector3 p0(0., 59., 145.), p1(0., 1., 0.);
+    //double xx = 0;
+    //auto vecX0tables1 = g->Start_Track(p0, p1, kFALSE,xx);
     //GuiTable::ShowX0guiTable(vecX0tables1);
+    //std::cout << "======> " << xx << std::endl;
+    auto h1 = g->GetXoverX0vsTheta();
+    auto myc = new TCanvas("myc", "myc", 800, 600);
+    myc->SetGrid();
+    h1->SetStats(kFALSE);
+    LansxFormat::FormatAll(h1, "%a %g", kMagenta, kMagenta);
+    h1->Draw("HIST");
+    // 
+
 
     //p1[1] = 1.; p1[2] = 2.;
     //auto vecX0table2 =g->Start_Track(p0, p1, kTRUE);
